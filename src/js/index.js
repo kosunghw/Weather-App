5b8a56c7dbc5117b4648db798d63dedd5124af0e
Sung <sung272012@hotmail.com>
2024-08-29 17:48:19 -0400

    Write functions that hit the API and process the JSON data, and log the weather information user searched

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 import '../style/main.scss';
+import * as apiFuncs from './api.js';
 
-console.log('hello');
+const apiKey = 'AZMSJW8MDWHLZ8WGESZARBBK2';
+const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/toronto/?key=${apiKey}`;
+const searchInput = document.querySelector('input');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  if (searchInput.value) {
+    const promise = apiFuncs.hitApi(searchInput.value);
+    apiFuncs.getWeatherData(promise);
+    form.reset();
+  }
+  e.preventDefault();
+});
+
+// const promise = apiFuncs.hitApi('toronto');
+// apiFuncs.processJSON(promise);
