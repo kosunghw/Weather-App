a547b7b3f1d71d90bde1de56905c6fb5d38cafa6
Sung <sung272012@hotmail.com>
2024-08-30 00:13:33 -0400

    modify getWeatherData function. It is now async function for a better readability. It returns an object containing the city's full name and the temperature.

--- src/js/api.js
+++ src/js/api.js
@@ -2,32 +2,17 @@ function hitApi(city) {
   const apiKey = 'AZMSJW8MDWHLZ8WGESZARBBK2';
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?key=${apiKey}`;
   return fetch(url, { mode: 'cors' });
-
-  promise.then((response) => {
-    cityName = response.address;
-    currentTemp = response.currentConditions.temp;
-    console.log(cityName);
-    console.log(currentTemp);
-  });
-
-  //   promise.then((response) => {
-  //     response.json().then((weather) => {
-  //       cityName = weather.address;
-  //       currentTemp = weather.temp;
-  //     });
-  //   });
-  //   console.log(cityName);
-  //   console.log(currentTemp);
 }
 
-function getWeatherData(promise) {
-  // This function takes in a promise, and process the JSON data.
+async function getWeatherData(promise) {
+  // This function takes in a promise by fetch(), and process the JSON data.
   // Returns an object that required for the app.
-  promise.then((response) => {
-    response.json().then((weatherData) => {
-      console.log(weatherData.currentConditions);
-    });
-  });
+  const weatherData = await promise.then((response) => response.json());
+  const condition = weatherData.currentConditions;
+  const cityFullName = weatherData.resolvedAddress;
+  const cityTemp = condition.temp;
+  //   console.log({ cityFullName, cityTemp });
+  return { cityFullName, cityTemp };
 }
 
 export { hitApi, getWeatherData };
