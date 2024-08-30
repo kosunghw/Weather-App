dbc673d41b6813a93aebbd10e4971e4dcc6b6633
Sung <sung272012@hotmail.com>
2024-08-30 01:00:07 -0400

    Shows icon of the weather

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 import '../style/main.scss';
 import * as apiFuncs from './api.js';
+import * as icon from './icon.js';
 
-const apiKey = 'AZMSJW8MDWHLZ8WGESZARBBK2';
 const searchInput = document.querySelector('input');
 const form = document.querySelector('form');
+const body = document.querySelector('body');
 
 const h1 = document.querySelector('#cityName');
 const p = document.querySelector('p');
+const iconDiv = document.getElementById('icon');
 
 form.addEventListener('submit', (e) => {
   if (searchInput.value) {
@@ -14,6 +16,7 @@ form.addEventListener('submit', (e) => {
     apiFuncs.getWeatherData(promise).then((response) => {
       h1.textContent = response.cityFullName;
       p.textContent = `${response.cityTemp} degrees Fahrenheit`;
+      iconDiv.innerHTML = icon.getIcon(response.icon);
     });
 
     form.reset();
